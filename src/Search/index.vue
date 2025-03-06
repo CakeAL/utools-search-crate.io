9e641c176d36c4b15872340406bc2b5a3e3a0484
CakeAL <1742507190@qq.com>
2025-03-06 19:44:26 +0800

    ✨ feat(search): 添加主屏默认提示，搜索中提示，失败提示

--- src/Search/index.vue
+++ src/Search/index.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { computed, ref, watch } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 import { Crate } from "../entity";
 
 const props = defineProps({
@@ -14,7 +14,21 @@ const props = defineProps({
 });
 const cratesData = ref<Crate[]>([]);
 const showCopied = ref(false);
+const showSearching = ref(false);
+const errorMessage = ref("");
+// const select = ref(0);
 let timeoutId: number | undefined;
+
+// onMounted(() => {
+//   window.addEventListener("keydown", (event) => {
+//     if (event.key === "ArrowDown") {
+//       select.value = Math.min(select.value + 1, cratesData.value.length - 1);
+//     } else if (event.key === "ArrowUp") {
+//       select.value = Math.max(select.value - 1, 0);
+//     }
+//     console.log(select.value);
+//   });
+// })
 
 watch(() => props.searchString, async (newSearchString) => {
   if (timeoutId) {
@@ -33,16 +47,23 @@ watch(() => props.searchString, async (newSearchString) => {
 const fetchCratesData = async (query: string) => {
   const url = `https://crates.io/api/v1/crates?page=1&per_page=50&q=${encodeURIComponent(query)}`;
   try {
+    showSearching.value = true;
     const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
       cratesData.value = data.crates;
     } else {
-      console.error('请求失败:', response.statusText);
+      // console.error('请求失败:', response.statusText);
+      errorMessage.value = `请求失败: ${response.statusText}`;
     }
   } catch (error) {
-    console.error('请求错误:', error);
+    // console.error('请求错误:', error);
+    errorMessage.value = `请求错误: ${error}`;
   }
+  showSearching.value = false;
+  setTimeout(() => {
+    errorMessage.value = "";
+  }, 3000);
 };
 
 const crateStableVersion = (crate: Crate) => {
@@ -68,6 +89,11 @@ const copyDependence = (crate: Crate) => {
 
 <template>
   <div>
+    <div v-if="cratesData.length === 0" class="label" style="top: 50vh">
+      <p>要搜索 Rust 包，请在上方输入框输入关键字</p>
+      <p>列表左侧鼠标单击可以复制包名和版本号 -> anyhow = "1"</p>
+      <p>点击右侧 🔗 可以跳转到该包详情页面</p>
+    </div>
     <div v-for="crate in cratesData" :key="crate.id" class="line-box">
       <div class="left-part" @click="copyDependence(crate)">
         <p class="first-line"><b>{{ crate.name }}</b> {{ crateStableVersion(crate) }} <span style="color: #D30300;">{{
@@ -79,24 +105,41 @@ const copyDependence = (crate: Crate) => {
         <div class="link-label" @click="openUrl(crate)">🔗</div>
       </span>
     </div>
-    <div v-if="showCopied" class="copied-toast">已复制</div>
+    <div v-if="showCopied" class="label copied-toast">已复制</div>
+    <div v-if="showSearching" class="label copied-toast">. . . 搜索中 . . .</div>
+    <div v-if="errorMessage !== ''" class="label" style="background-color: rgba(255, 82, 82, 0.7);">{{ errorMessage }}</div>
   </div>
 </template>
 
 <style>
+.label {
+  text-align: center;
+  position: fixed;
+  top: 80vh;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: rgba(0, 125, 42, 0.7);
+  color: white;
+  padding: 5px 10px;
+  border-radius: 5px;
+  font-size: 14px;
+  white-space: nowrap;
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 20px 30px;
+}
+
 .line-box {
   display: flex;
   justify-content: space-between;
   align-items: center;
   height: calc(100vh / 10);
   min-height: 50px;
-  padding: 0 5px;
   box-shadow: inset 0 0 0 1px #ccc;
 }
 
 .left-part {
   width: 70%;
   height: 50px;
+  padding-left: 5px;
 }
 
 .left-part:hover {
@@ -144,16 +187,6 @@ const copyDependence = (crate: Crate) => {
 }
 
 .copied-toast {
-  position: absolute;
-  top: 80%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: rgba(0, 125, 42, 0.7);
-  color: white;
-  padding: 5px 10px;
-  border-radius: 5px;
-  font-size: 14px;
-  white-space: nowrap;
   animation: fadeOut 1.5s ease-in-out;
 }
 
